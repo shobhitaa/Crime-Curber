ec88ab94a5e057058600a4a87a8fc9581363b3ff
shobhitaa <shobha2738@gmail.com>
2020-10-31 01:07:49 +0530

    Modified main screen
    - resized cards
    - add icons
    - add buttons

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,4 +4,5 @@ const light_gray = Color(0xFFEFEFEF);
 const dark_charcoal = Color(0xFF333333);
 const link_blue = Color(0xFF195FAB);
 const light_gray_bg = Color(0xFFECECEC);
+const text_gray = Color(0xFF9D9D9D);
 
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -7,6 +7,7 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+  bool _hasBeenPressed = false;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -30,32 +31,66 @@ class _MainViewState extends State<MainView> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Container(
-                  height: 120,
-                  width: 100,
+                  height: 200,
+                  width: 120,
                   child: Card(
                       color: Colors.white,
+                      elevation: 8,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(17)),
                       child: Column(
-                        children: <Widget>[Text("Record")],
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Image.asset("assets/images/dslr-camera.png",
+                          width: 70,
+                          height: 70),
+                          Text("Record",
+                          style: TextStyle(
+                            color: dark_charcoal,
+                            fontFamily: "OpenSans",
+                            fontSize: 17
+                          ))],
                       )),
                 ),
                 Column(
                   children: <Widget>[
                     Container(
-                      height: 60,
-                      width: 100,
+                      height: 100,
+                      width: 110,
                       child: Card(
                           color: Colors.white,
+                          elevation: 8,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(17)),
                           child: Column(
-                            children: <Widget>[Text("Hospital")],
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[Image.asset("assets/images/hospital.png",
+                            width: 40,
+                            height: 40),
+                              Text("Hospital",
+                                  style: TextStyle(
+                                      color: dark_charcoal,
+                                      fontFamily: "OpenSans",
+                                      fontSize: 14
+                                  ))],
                           )),
                     ),
                     Container(
-                      height: 60,
-                      width: 100,
+                      height: 100,
+                      width: 110,
                       child: Card(
                           color: Colors.white,
+                          elevation: 8,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(17)),
                           child: Column(
-                            children: <Widget>[Text("SOS Timer")],
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[Image.asset("assets/images/sostimer.png",
+                                width: 40,
+                                height: 40),
+                              Text("SOS Timer",
+                                  style: TextStyle(
+                                      color: dark_charcoal,
+                                      fontFamily: "OpenSans",
+                                      fontSize: 14
+                                  ))],
                           )),
                     )
                   ],
@@ -63,37 +98,173 @@ class _MainViewState extends State<MainView> {
                 Column(
                   children: <Widget>[
                     Container(
-                      height: 60,
-                      width: 100,
+                      height: 100,
+                      width: 110,
                       child: Card(
                           color: Colors.white,
+                          elevation: 8,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(17)),
                           child: Column(
-                            children: <Widget>[Text("Police")],
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[Image.asset("assets/images/police.png",
+                                width: 40,
+                                height: 40),
+                              Text("Police",
+                                  style: TextStyle(
+                                      color: dark_charcoal,
+                                      fontFamily: "OpenSans",
+                                      fontSize: 14
+                                  ))],
                           )),
                     ),
                     Container(
-                      height: 60,
-                      width: 100,
+                      height: 100,
+                      width: 110,
                       child: Card(
                           color: Colors.white,
+                          elevation: 8,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(17)),
                           child: Column(
-                            children: <Widget>[Text("Share Location")],
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[Image.asset("assets/images/location.png",
+                                width: 40,
+                                height: 40),
+                              Text("Share Location",
+                                  style: TextStyle(
+                                      color: dark_charcoal,
+                                      fontFamily: "OpenSans",
+                                      fontSize: 14
+                                  ))],
                           )),
                     )
                   ],
                 )
               ],
             ),
-            OutlineButton(
-              shape: RoundedRectangleBorder(
-                borderRadius:BorderRadius.circular(50)
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: CircleBorder(),
+                  primary: _hasBeenPressed ? Colors.red : Colors.blueGrey,
+                  elevation: 7
               ),
-              onPressed: null,
-              child: Column(
-                children: <Widget>[
-                  Text("SOS"),
-                ],
+              onPressed: () {
+                setState(() {
+                  _hasBeenPressed = !_hasBeenPressed;
+                });
+              },
+              child: Container(
+                width: 200,
+                height: 200,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(shape: BoxShape.circle),
+                child: Text(
+                  'SOS',
+                  style: TextStyle(fontSize: 44),
+                ),
               ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: CircleBorder(),
+                          primary: Colors.white,
+                          elevation: 7
+                      ),
+                      onPressed: () {},
+                      child: Container(
+                          width: 63,
+                          height: 63,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(shape: BoxShape.circle),
+                          child: //Image.asset("assets/images/add-contact.png")
+                          Text("C",
+                            style: TextStyle(
+                                color: Colors.black
+                            ),)
+                      ),
+                    ),
+                    Text("Emergency",
+                        style: TextStyle(
+                            color: dark_charcoal,
+                            fontFamily: "OpenSans",
+                            fontSize: 12,
+                            decoration: TextDecoration.none
+                        )
+                    ),
+                    Text("Contacts",
+                        style: TextStyle(
+                            color: dark_charcoal,
+                            fontFamily: "OpenSans",
+                            fontSize: 12,
+                            decoration: TextDecoration.none
+                        )
+                    )
+                  ],
+                ),
+                Column( children: <Widget>[
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(),
+                        primary: Colors.white,
+                        elevation: 7
+                    ),
+                    onPressed: () {},
+                    child: Container(
+                        width: 63,
+                        height: 63,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(shape: BoxShape.circle),
+                        child: Image.asset("assets/icons/upload.png",
+                            width: 41,
+                            height: 41
+                        )
+                    ),
+                  ),
+                  Text("Upload",
+                      style: TextStyle(
+                          color: dark_charcoal,
+                          fontFamily: "OpenSans",
+                          fontSize: 12,
+                          decoration: TextDecoration.none
+                      ))
+                ]
+                ),
+                Column(
+                  children: <Widget>[
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: CircleBorder(),
+                          primary: Colors.white,
+                          elevation: 7
+                      ),
+                      onPressed: () {},
+                      child: Container(
+                          width: 63,
+                          height: 63,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(shape: BoxShape.circle),
+                          child: Image.asset("assets/icons/help.png",
+                              width: 40,
+                              height: 40)
+                      ),
+                    ),
+                    Text(
+                      "Help",
+                      style: TextStyle(
+                          color: dark_charcoal,
+                          fontFamily: "OpenSans",
+                          fontSize: 12,
+                          decoration: TextDecoration.none
+                    ))
+                  ],
+                )
+
+
+              ],
             )
           ],
         ));
